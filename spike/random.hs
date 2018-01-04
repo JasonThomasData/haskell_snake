26ea8d429bce14cde6a0553b6fd1c60079990c17
john smith <jason.robert.thomas.123@gmail.com>
2018-01-04 17:04:12 +1100

    Updating spiky spikes

--- spike/random.hs
+++ spike/random.hs
@@ -2,14 +2,23 @@ import System.Random (randomRIO)
 
 randomPos :: (Int, Int) -> IO (Int, Int)
 randomPos board_limits = do
-    row_number <- randomRIO (0, (fst board_limits) :: Int) :: IO Int
-    col_number <- randomRIO (0, (snd board_limits) :: Int) :: IO Int
+    row_number <- randomRIO (0, fst board_limits :: Int) :: IO Int
+    col_number <- randomRIO (0, snd board_limits :: Int) :: IO Int
     return (row_number, col_number)
+
+checkValid :: (Int, Int) -> IO Bool
+checkValid pos
+    | pos == (1, 1) = return True
+    | otherwise     = return False
 
 getResults :: IO ()
 getResults = do
-    let board_limits = (500,500)
-    randomPos board_limits >>= print
+    let board_limits = (2, 2)
+    pos <- randomPos board_limits
+    valid <- checkValid pos
+    if valid
+        then print "HIT" >> print pos 
+        else print "MISS" >> print pos
 
 main = getResults
 
