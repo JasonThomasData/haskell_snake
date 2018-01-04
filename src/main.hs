6e36e7604e4a1270386716efa1165221eaaa8921
john smith <jason.robert.thomas.123@gmail.com>
2018-01-05 00:23:37 +1100

    Final logic for gameLoop, logic for gameFinish

--- src/main.hs
+++ src/main.hs
@@ -1,22 +1,21 @@
---Add targets to board
---Update view with targets
---On touching snake or edge, kill snake, end game
-
 import System.IO
 import Control.Concurrent (threadDelay)
 import System.Console.ANSI (cursorUpLine)
+import System.Exit (exitSuccess)
 
-import InOut (createDisplayBuffer, displayBoard, getKeyPressed, setBufferSettings, getDirection, displayInstructions)
-import Snake (eatTarget, updateSnake)
+import Interact (updateTargets, getVacantRandomPos)
+import InOut (createDisplayBuffer, displayBoard, getKeyPressed, setBufferSettings, displayInstructions, toTopOfDisplay)
+import Snake (checkAlive, eatObject, updateSnake, getDirection)
 import View (createBoard)
 
 startGame :: IO ()
 startGame = do
-    let snake = [(10, 10)] :: [(Int,Int)]
+    let snake = [(12, 12)] :: [(Int,Int)]
     let direction = (0, -1) :: (Int,Int)
-    let board_limits = (25, 25) :: (Int,Int)
-    let targets = [] :: [(Int,Int)]
-    let delay = 300000 :: Int
+    let board_limits = (14, 14) :: (Int,Int)
+    firstTarget <- getVacantRandomPos board_limits snake
+    let targets = [firstTarget] :: [(Int,Int)]
+    let delay = 220000 :: Int
     setBufferSettings
     displayInstructions
     createDisplayBuffer (snd board_limits)
@@ -26,16 +25,25 @@ gameLoop :: [(Int,Int)] -> (Int,Int) -> (Int,Int) -> [(Int,Int)] -> Int -> IO ()
 gameLoop snake current_direction board_limits targets delay = do
     key_down <- getKeyPressed
     let new_direction = getDirection key_down current_direction
-    let has_eaten = eatTarget snake targets
+    let has_eaten = eatObject (head snake) targets
     let updated_snake = updateSnake snake new_direction has_eaten
-    render updated_snake board_limits targets
+    updated_targets <- updateTargets snake targets board_limits has_eaten
+    let is_alive = checkAlive updated_snake board_limits
+    render updated_snake board_limits updated_targets
     threadDelay delay
-    gameLoop updated_snake new_direction board_limits targets delay
+    if is_alive
+        then gameLoop updated_snake new_direction board_limits updated_targets delay
+        else gameFinish
+
+gameFinish :: IO ()
+gameFinish = do
+    print "You are a dead snake"
+    exitSuccess
 
 render :: [(Int,Int)] -> (Int,Int) -> [(Int,Int)] -> IO ()
 render snake board_limits targets = do
+    toTopOfDisplay board_limits
     let board_view = createBoard [] (fst board_limits) (snd board_limits) snake targets
-    cursorUpLine (fst board_limits)
     displayBoard board_view
 
-main = start_game
+main = startGame
